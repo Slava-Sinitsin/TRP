3ae073e02c324224b3a7ba6fcca6eca785b02ecb
Slava-Sinitsin <slav-ian@mail.ru>
2023-06-22 02:20:57 +0700

    Made my own theme

--- app/src/main/java/com/example/trp/ui/theme/Color.kt
+++ app/src/main/java/com/example/trp/ui/theme/Color.kt
@@ -2,12 +2,24 @@ package com.example.trp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val lightScheme = TRPColors(
+    primaryBackground = Color(0xFFFFFFFF),
+    primaryText = Color(0xFF3D454C),
+    secondaryBackground = Color(0xFFF3F4F5),
+    secondaryText = Color(0xFF3D454C),
+    tintColor = Color.Magenta,
+    controlColor = Color(0xFF7A8A99),
+    errorColor = Color(0xFF000000),
+    MyYellow = Color(0xFFE2B714)
+)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val MyYellow = Color(0xFFE2B714)
+val darkScheme = TRPColors(
+    primaryBackground = Color(0xFF242424),
+    primaryText = Color(0xFFF2F4F5),
+    secondaryBackground = Color(0xFF1A1A1A),
+    secondaryText = Color(0xFF3D454C),
+    tintColor = Color.Magenta,
+    controlColor = Color(0xFF7A8A99),
+    errorColor = Color(0xFFFF6699),
+    MyYellow = Color(0xFFE2B714)
+)
